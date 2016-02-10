73aa7299d33522ce6439fc04239d06e343e06321
mFizzzle <matthewdf14@gmail.com>
2016-02-10 15:04:46 -0600

    blackjack002

---
+++ app/src/main/java/com/example/student/myapplication/GetterSetter.java
@@ -0,0 +1,15 @@
+package com.example.student.myapplication;
+
+/**
+ * Created by mfizzle on 1/28/16.
+ */
+public class GetterSetter {
+
+    static float touchX = 0.0f;
+    static float touchY = 0.0f;
+    static float size = 20f;
+    static int paintRed = 255;
+    static int paintBlue = 0;
+    static int paintGreen = 0;
+
+}
